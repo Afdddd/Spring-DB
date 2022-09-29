dffb638282be349ae18e8b10fa0ff83d4e803b37
inyeop <kimdlsduq36@gmail.com>
2022-09-29 22:48:06 +0900

    JDBC Connection

---
+++ src/test/java/hello/jdbc/Connection/DBConnectionUtilTest.java
@@ -0,0 +1,16 @@
+package hello.jdbc.Connection;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+public class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        Assertions.assertThat(connection).isNotNull();
+    }
+}
